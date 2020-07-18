124e5c216f2d0a98276aa39ab40e1f5851bbd46d
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-18 14:17:03 +0200

    CREATE LogInActivity

---
+++ app/src/main/java/com/shalaga44/bloodbank/LogInActivity.kt
@@ -0,0 +1,11 @@
+package com.shalaga44.bloodbank
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
