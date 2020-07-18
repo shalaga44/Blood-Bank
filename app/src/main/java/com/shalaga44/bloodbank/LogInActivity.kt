552335556e255598e0abc4dff48d2401515f1545
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-07-18 15:50:19 +0200

    SETUP Log in Default ActionBar With NavController

--- app/src/main/java/com/shalaga44/bloodbank/LogInActivity.kt
+++ app/src/main/java/com/shalaga44/bloodbank/LogInActivity.kt
@@ -1,11 +1,35 @@
 package com.shalaga44.bloodbank
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class LogInActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+
+        setupDefaultActionBarWithNavController()
+
+    }
+
+    private fun setupDefaultActionBarWithNavController() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.login_nav_host_fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
 }
